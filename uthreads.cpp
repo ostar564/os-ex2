323d98264f0d687faa3f716e25199cd96003957b
TLP-291 <ido.maymon.44@gmail.com>
2024-06-10 19:29:06 +0300

    implemented my 4 functions

--- uthreads.cpp
+++ uthreads.cpp
@@ -8,15 +8,30 @@ typedef struct TCB
     unsigned int tid;
     sigjmp_buf env;
     unsigned int quantums;
+    unsigned int to_sleep;
+    bool paused;
 } TCB;
 
 std::deque<TCB *> ready;
 std::set<unsigned int> freeIds;
-std::map<unsigned int, TCB> blocked;
+std::map<unsigned int, TCB *> blocked;
 TCB *running;
+int quantum_time;
 
 void timer_handler(int sig)
 {
+  for (auto &it = blocked.begin(); it != blocked.end(); it++)
+    {
+      if (it -> second -> to_sleep != 0)
+        {
+          if (it -> second -> to_sleep-- == 0 && !paused)
+            {
+              blocked.erase(it -> second -> to_sleep);
+              ready.push_back(it -> second -> to_sleep);
+            }
+
+        }
+    }
   if (!ready.empty())
   {
     TCB *current = running;
@@ -44,6 +59,8 @@ int uthread_init(int quantum_usecs)
   TCB *main_thread;
   main_thread -> tid = 0;
   main_thread -> quantums = 1; // TODO: Reassure this starts at 1
+  main_thread -> to_sleep = 0;
+  main_thread -> paused = false;
 
   sigsetjmp(main_thread -> env, 1);
   sigemptyset(main_thread -> env -> __saved_mask);
@@ -59,6 +76,7 @@ int uthread_init(int quantum_usecs)
       printf("sigaction error.");
     }
 
+    quantum_time = quantum_usecs;
   // Configure the timer to expire after 1 sec... */
   timer.it_value.tv_sec = quantum_usecs / (1e6);        // first time interval, seconds part
   timer.it_value.tv_usec = quantum_usecs % (1e6);        // first time interval, microseconds part
@@ -73,4 +91,75 @@ int uthread_init(int quantum_usecs)
       printf("setitimer error.");
     }
 
+}
+
+int uthread_block(int tid)
+{
+  // TODO: Check what happens if ready is empty
+  if (tid == running -> tid)
+    {
+      TCB *current = running;
+      int ret_val = sigsetjmp(current -> env, 1);
+      if (ret_val == 0)
+        {
+          blocked[tid] = current;
+          current -> paused = true;
+          current = ready.pop_front ();
+          current->quantums++;
+          running = current;
+          siglongjmp (current->env, 1);
+        }
+    }
+    else
+    {
+      for (const auto& elem : ready)
+        {
+          if (elem -> tid == tid)
+            {
+              blocked[tid] = elem;
+              ready.erase(elem);
+              return 0;
+            }
+        }
+        std::cerr << "thread library error: No thread with ID " << tid << std::endl;
+      return -1;
+    }
+}
+
+int uthread_resume(int tid)
+{
+  TCB *to_resume = blocked.find(tid);
+  if (to_resume != blocked.end())
+    {
+      to_resume -> paused = false;
+      if (to_resume -> to_sleep == 0)
+        {
+          blocked.erase (to_resume);
+          ready.push_back (to_resume);
+        }
+    }
+    else
+    {
+      if (running -> tid == tid)
+        return 0;
+      for (const auto& elem : ready)
+          if (elem -> tid == tid)
+            return 0;
+      return -1;
+    }
+}
+
+int uthread_sleep(int num_quantums)
+{
+  blocked[tid] = running;
+  running -> to_sleep = num_quantums;
+  running = ready.pop_front ();
+  running->quantums++;
+  siglongjmp (running->env, 1);
+
+}
+
+int uthread_get_tid()
+{
+  return running -> tid;
 }
